c49c4f2dfcf3f4860e022e1bb09b773c4df45ce6
catymiao <33770231+catymiao@users.noreply.github.com>
2019-05-01 19:10:46 -0700

    Add files via upload

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -5,17 +5,13 @@
 #include "utility.h"
 
 int countPrimes(int a[], int size) {
-	int numPrimes = 0; 
-	int count = 0; 
-	for (int i=0; i<size; i++){
-		for (int j=2; j<a[i] ; j++){
-			if (a[i]%j == 0){
-				count++; 
-			}
-		}
-	if (count == 0){
-		numPrimes++; 
-	}
-	}
-  return numPrimes; // STUB!  Replace with correct code.
+  int count = 0;
+
+  for (int i = 0; i < size; i++) {
+  	if (isPrime(a[i]) == true) {
+  		count += 1;
+  	}
+  }
+
+  return count;
 }
